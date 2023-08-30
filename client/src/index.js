a19f47ef75da25b17f59d5036956db25b22f1f2b
mobarakamir17 <mobarakamir17@gmail.com>
2023-08-29 20:24:48 -0400

    left side mostly complete, right side functionality, added tasks, and logo in browser

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
-import React from "react";
-import App from "./components/App";
-import "./style.css";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './style.css';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import Bootstrap JS dependencies
+import 'jquery/dist/jquery.slim.min.js';
+import 'popper.js/dist/umd/popper.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
