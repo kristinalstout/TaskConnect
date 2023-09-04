63539e822549e8923202d5e528e21e671a4134da
mobarakamir17 <mobarakamir17@gmail.com>
2023-09-04 18:36:24 -0400

    formik form complete, removed profile login, working on tasks functionality with backend

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -2,28 +2,50 @@ import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 function Tasks({ tasks, setTasks }) {
-  const location = useLocation(); // Get the current route location
+  const location = useLocation(); 
 
-  const handleStatusClick = (taskId) => {
-    // Your existing code for updating task status
+ const handleStatusClick = (taskId) => {
+    let updatedTasks = tasks.map((task) => {
+      if (task.id === taskId){
+        return {...task, status: !task.status}
+      };
+      return task;
+    });
+    setTasks(updatedTasks)
   }
 
-  function taskStatus(status) {
-    // Your existing code for displaying task status
+  function taskStatus(status){
+    const displayStatus = status ===true? "Completed" : "Not Started"
+    return displayStatus
   }
 
   const convertedDates = tasks.map(obj => ({ ...obj, dueDate: new Date(obj.dueDate) }));
-  const sortedDates = convertedDates.sort((a, b) => b.date - a.date);
+  const sortedDates = convertedDates.sort((a, b) => b.dueDate - a.dueDate);
+
+  const handleDeleteClick = (taskId) => {
+    const taskIndex = tasks.findIndex(task => task.id === taskId);
+    if (taskIndex !== -1) {
+      const updatedTasks = [...tasks.slice(0, taskIndex), ...tasks.slice(taskIndex + 1)];
+      setTasks(updatedTasks);
+    }
+  };
 
   return (
     <div className="task_page">
-      {location.pathname === '/tasks' && <h1>Task List</h1>} {/* Conditionally render the header */}
+      {location.pathname === '/tasks' && <h1>Task List</h1>}
       <ul>
-        {sortedDates.map((task) => (
+        {tasks.map((task) => (
           <li key={task.id}>
-            {task.task} - {taskStatus(task.status)}
+              <strong>Task:</strong> {task.task}<br />
+            <strong>Due Date:</strong> {task.dueDate}<br />
+            <strong>Assignee:</strong> {task.assignee}<br />
+            <strong>Space:</strong> {task.space}<br />
+            <strong>Status:</strong> {taskStatus(task.status)}
             <button onClick={() => handleStatusClick(task.id)}>
               Mark as Completed
+            </button>
+            <button onClick={() => handleDeleteClick(task.id)}>
+              Delete
             </button>
           </li>
         ))}
